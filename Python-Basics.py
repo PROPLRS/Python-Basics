58e2f3c516c11c8ee52547f54950da9bd7c12c9f
PROPLRS <sanupocket@gmail.com>
2025-06-12 13:28:37 +0200

    Added profile generator

--- Python-Basics.py
+++ Python-Basics.py
@@ -1,1 +1,7 @@
 print("Hello from Shrish!")
+
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+hobby = input("What is your hobby: ")
+
+print(f"\nHello {name} !\nYou are {age} years old.\nYour hobby is {hobby} which is very interesting!")
