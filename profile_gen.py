f09ce6230534250d3719d5d098d7477daa093873
PROPLRS <sanupocket@gmail.com>
2025-06-12 13:40:57 +0200

    Hi to everyone

---
+++ profile_gen.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+hobby = input("What is your hobby: ")
+
+print(f"\nHello {name} !\nYou are {age} years old.\nYour hobby is {hobby} which is very interesting!")
